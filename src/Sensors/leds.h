2d2bddd3e1607de3be6b4ef29f832483351284f7
Andria Inasaridze <andria.inas@gmail.com>
2025-11-17 13:21:01 +0100

    Fix case conflict: remove sensor folders

--- src/Sensors/leds.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <zephyr/drivers/gpio.h>
-#include <stdbool.h>
-
-int  leds_init(void);
-void rgb_set(bool r, bool g, bool b);
--- src/sensors/accelerometer_sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACCELEROMETER_SENSOR_H
-#define ACCELEROMETER_SENSOR_H
-
-#include <stdint.h>
-
-int accelerometer_sensor_init(void);
-int accelerometer_sensor_read(int32_t *x_g100, int32_t *y_g100, int32_t *z_g100);
-
-#endif /* ACCELEROMETER_SENSOR_H */
